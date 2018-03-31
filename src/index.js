0081b98fbd64af2b5301954f6a03a50d02ee5e58
Sesan.Osanyinbi <sesan.osanyinbi@venturegardengroup.com>
2018-03-31 02:18:36 +0100

    New update of my starter pack

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
-// Create a new component. THis component should produce some html
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
-const App = function(){
-  return <div>Hi</div>
-}
+import App from './components/app';
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>
+  , document.querySelector('.container'));
